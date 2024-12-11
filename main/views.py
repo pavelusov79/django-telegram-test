283126748d26589e9702496d8b0c3851ef2decba
pavelusov79 <pavel.usov79@gmail.com>
2024-12-11 11:54:29 +1000

    11.12.24

--- main/views.py
+++ main/views.py
@@ -1,20 +1,39 @@
+import json
+
+import redis
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.views.generic import TemplateView
+
+
+r = redis.Redis(host='localhost', port=6379, db=0)
 
 
 class MainView(TemplateView):
     template_name = 'main/index.html'
 
     def get(self, request, *args, **kwargs):
-        if request.GET.get('id'):
-            username = request.GET.get('u')
-            u_pass = request.GET.get('id')
-            name = request.GET.get('name')
-            user = authenticate(username=username, password=u_pass)
+        # Вариант 1
+        # if request.GET.get('id'):
+        #     username = request.GET.get('u')
+        #     u_pass = request.GET.get('id')
+        #     name = request.GET.get('name')
+        #     user = authenticate(username=username, password=u_pass)
+        #     if user:
+        #         login(request, user)
+        #     else:
+        #         user = User.objects.create_user(username=username, email=None, password=u_pass, first_name=name)
+        #         login(request, user)
+
+        # Вариант 2
+        if r.get('data_user'):
+            data = json.loads(r.get('data_user'))
+            user = authenticate(username=data['u'], password=data['id'])
             if user:
                 login(request, user)
             else:
-                user = User.objects.create_user(username=username, email=None, password=u_pass, first_name=name)
+                user = User.objects.create_user(
+                    username=data['u'], email=None, password=str(data['id']), first_name=data['name']
+                )
                 login(request, user)
         return super().get(request, *args, **kwargs)
--- telegram.py
+++ telegram.py
@@ -1,17 +1,28 @@
+import json
+import webbrowser
+
 import telebot
+import redis
 from telebot import types
 from django_telegram.settings import BOT_TOKEN
 
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
+# вариант №1. Передать данные как get параметры в url и затем уже в джанго их вывести
+# btn 1 = types.InlineKeyboardButton(
+#         'Авторизоваться', url=f'http://127.0.0.1:8000/?id={message.from_user.id}&name={message.from_user.first_name}&'
+#                               f'u={message.from_user.username}
+# )
+# вариант №2. Используем redis. Данные кешируем и затем в джанго обращаемся к redis кэшу и авторизовываем пользователя.
+
 
 @bot.message_handler(commands=['start'])
 def main(message):
+
     markup = types.InlineKeyboardMarkup()
     btn1 = types.InlineKeyboardButton(
-        'Авторизоваться', url=f'http://127.0.0.1:8000/?id={message.from_user.id}&name={message.from_user.first_name}&'
-                              f'u={message.from_user.username}'
+        'Авторизоваться', callback_data='auth'
     )
     btn2 = types.InlineKeyboardButton('Отмена', callback_data='delete')
     markup.add(btn1, btn2)
@@ -24,8 +35,16 @@ def main(message):
 def callback_func(callback):
     if callback.data == 'delete':
         bot.delete_message(callback.message.chat.id, callback.message.message_id)
-    else:
+    elif callback.data == 'auth':
+        data_user = {
+            'id': callback.message.chat.id,
+            'name': callback.message.chat.first_name,
+            'u': callback.message.chat.username
+        }
+        r = redis.Redis(host='localhost', port=6379, db=0)
+        r.set('data_user', json.dumps(data_user), 120)
         bot.send_message(callback.message.chat.id, 'Вы успешно авторизовались на сайте.')
+        webbrowser.open('http://127.0.0.1:8000')
 
 
 bot.polling(none_stop=True)
